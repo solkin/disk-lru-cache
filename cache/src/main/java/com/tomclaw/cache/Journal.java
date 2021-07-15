a16d789b6185916392cfad1ea5bcfb838ad70b2c
Igor Solkin <inbox@tomclaw.com>
2021-07-15 15:01:39 +0300

    Fix journal finalizers; Upgrade demo app dependencies. Closes #4

--- cache/src/main/java/com/tomclaw/cache/Journal.java
+++ cache/src/main/java/com/tomclaw/cache/Journal.java
@@ -4,6 +4,7 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -103,24 +104,20 @@ class Journal {
     }
 
     public void writeJournal() {
-        DataOutputStream stream = null;
-        try {
-            stream = new DataOutputStream(new FileOutputStream(file));
-            stream.writeShort(JOURNAL_FORMAT_VERSION);
-            stream.writeInt(map.size());
-            for (Record record : map.values()) {
-                stream.writeUTF(record.getKey());
-                stream.writeUTF(record.getName());
-                stream.writeLong(record.getTime());
-                stream.writeLong(record.getSize());
-            }
-        } catch (IOException ex) {
-            if (stream != null) {
-                try {
-                    stream.close();
-                } catch (IOException ignored) {
+        try (FileOutputStream fileStream = new FileOutputStream(file)) {
+            try (DataOutputStream stream = new DataOutputStream(fileStream)) {
+                stream.writeShort(JOURNAL_FORMAT_VERSION);
+                stream.writeInt(map.size());
+                for (Record record : map.values()) {
+                    stream.writeUTF(record.getKey());
+                    stream.writeUTF(record.getName());
+                    stream.writeLong(record.getTime());
+                    stream.writeLong(record.getSize());
                 }
             }
+        } catch (IOException ex) {
+            log("[.] Failed to write journal %s", ex.getMessage());
+            ex.printStackTrace();
         }
     }
 
@@ -128,35 +125,31 @@ class Journal {
         File file = fileManager.journal();
         log("[.] Start journal reading", file.getName());
         Journal journal = new Journal(file, fileManager);
-        DataInputStream stream = null;
-        try {
-            stream = new DataInputStream(new FileInputStream(file));
-            int version = stream.readShort();
-            if (version != JOURNAL_FORMAT_VERSION) {
-                throw new IllegalArgumentException("Invalid journal format version");
-            }
-            int count = stream.readInt();
-            long totalSize = 0;
-            for (int c = 0; c < count; c++) {
-                String key = stream.readUTF();
-                String name = stream.readUTF();
-                long time = stream.readLong();
-                long size = stream.readLong();
-                totalSize += size;
-                Record record = new Record(key, name, time, size);
-                journal.put(record);
-            }
-            journal.setTotalSize(totalSize);
-            log("[.] Journal read. Files count is %d and total size is %d", count, totalSize);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        } finally {
-            if (stream != null) {
-                try {
-                    stream.close();
-                } catch (IOException ignored) {
+        try (FileInputStream fileStream = new FileInputStream(file)) {
+            try (DataInputStream stream = new DataInputStream(fileStream)) {
+                int version = stream.readShort();
+                if (version != JOURNAL_FORMAT_VERSION) {
+                    throw new IllegalArgumentException("Invalid journal format version");
                 }
+                int count = stream.readInt();
+                long totalSize = 0;
+                for (int c = 0; c < count; c++) {
+                    String key = stream.readUTF();
+                    String name = stream.readUTF();
+                    long time = stream.readLong();
+                    long size = stream.readLong();
+                    totalSize += size;
+                    Record record = new Record(key, name, time, size);
+                    journal.put(record);
+                }
+                journal.setTotalSize(totalSize);
+                log("[.] Journal read. Files count is %d and total size is %d", count, totalSize);
             }
+        } catch (FileNotFoundException ignored) {
+            log("[.] Journal not found and will be created");
+        } catch (IOException ex) {
+            log("[.] Failed to read journal %s", ex.getMessage());
+            ex.printStackTrace();
         }
         return journal;
     }
