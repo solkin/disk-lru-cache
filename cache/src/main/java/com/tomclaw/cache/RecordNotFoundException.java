38b94410b87d5c3898961f3d38b54488db678d0a
Igor Solkin <igor@citron.dev>
2026-01-19 03:03:55 +0300

    Change RecordNotFoundException to extend Exception

--- cache/src/main/java/com/tomclaw/cache/RecordNotFoundException.java
+++ cache/src/main/java/com/tomclaw/cache/RecordNotFoundException.java
@@ -1,5 +1,10 @@
 package com.tomclaw.cache;
 
 @SuppressWarnings("WeakerAccess")
-public class RecordNotFoundException extends Throwable {
+public class RecordNotFoundException extends Exception {
+
+    public RecordNotFoundException() {
+        super("Record not found in cache");
+    }
+
 }
